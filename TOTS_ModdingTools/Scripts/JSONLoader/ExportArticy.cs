96935da1a3286de8597318e0423a6f8e099bf5d5
James <james.veugelaers@outlook.co.nz>
2026-04-26 19:06:52 +1200

    Exporting now dumps string tables

--- TOTS_ModdingTools/Scripts/JSONLoader/ExportArticy.cs
+++ TOTS_ModdingTools/Scripts/JSONLoader/ExportArticy.cs
@@ -1,9 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using Articy.Unity;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using TOTS_ModdingTools;
+using TotS.Utils;
 using UnityEngine;
 
 public class ExportArticy
@@ -36,6 +40,74 @@ public class ExportArticy
                 File.WriteAllText(file, json);
             }
 
+        }
+
+        
+        ExportStringTables();
+        
+        GC.Collect(); // Because we create way too much garbage here
+    }
+
+    private static void ExportStringTables()
+    {
+        ArticyLocalizationPackage[] packages = Resources.LoadAll<ArticyLocalizationPackage>("");
+        
+        Dictionary<string, List<(string, ArticyLocalizationPackage)>> stringTables = new Dictionary<string, List<(string, ArticyLocalizationPackage)>>();
+        foreach (ArticyLocalizationPackage package in packages)
+        {
+            string packageName = package.name;
+            int indexOf = packageName.LastIndexOf("_");
+            string tableName = packageName.Substring(0, indexOf);
+            string languageCode = packageName.Substring(indexOf + 1, packageName.Length - indexOf - 1);
+            if (!stringTables.ContainsKey(tableName))
+            {
+                stringTables.Add(tableName, new List<(string, ArticyLocalizationPackage)>());
+            }
+            
+            stringTables[tableName].Add((languageCode, package));
+        }
+        APILogger.LogInfo($"Exporting {packages.Length} packages as {stringTables.Count} stringTables");
+
+
+        foreach (KeyValuePair<string, List<(string, ArticyLocalizationPackage)>> table in stringTables)
+        {
+            HashSet<string> keys = new HashSet<string>();
+            foreach ((string, ArticyLocalizationPackage) pair in table.Value)
+            {
+                keys.AddRange(pair.Item2.LocaKeys);
+            }
+            
+            string[] sortedKeys = keys.OrderBy(a => a).ToArray();
+            CSVBuilder builder = new CSVBuilder();
+            foreach (string key in sortedKeys)
+            {
+                builder.AddValue("Key", key, 0);
+
+                for (var i = 0; i < table.Value.Count; i++)
+                {
+                    var (languageCode, package) = table.Value[i];
+                    int index = package.mLocaKeys.IndexOf(key);
+                    if (index >= 0)
+                    {
+                        builder.AddValue(languageCode, package.mLocaValues[index], 1 + i);
+                    }
+                    else
+                    {
+                        builder.AddValue(languageCode, "", 1 + i);
+                    }
+                }
+                builder.NextRow();
+            }
+            
+            string directory = Path.Combine(ModdingToolsPlugin.ExportPath, "StringTables");
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            
+            string stringTablePath = Path.Combine(directory, $"{table.Key}.csv");
+            builder.SaveAsCSV(stringTablePath);
+            APILogger.LogInfo("StringTable saved to: " + stringTablePath);
         }
     }
 }
